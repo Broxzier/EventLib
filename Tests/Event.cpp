c274b773e841babf9ef7f467de06a10fa984d5c5
Broxzier <hielke.morsink@gmail.com>
2017-06-18 12:03:33 +0200

    Add unit test for connecting simultaneously

--- Tests/Event.cpp
+++ Tests/Event.cpp
@@ -1,6 +1,7 @@
 #include "CppUnitTest.h"
 
 #include <EventLib/Event.hpp>
+#include <thread>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -41,6 +42,32 @@ public:
 		Signal();
 
 		Assert::AreEqual(i, 1); // i == 1
+	}
+
+	TEST_METHOD(ConnectSimultaneously)
+	{
+		int  i = 0;
+		auto IncrementI = [&i]() -> void { i++; };
+
+		el::Event<void()> Signal;
+
+		// Run two threads that connect a function to a shared event
+		std::thread ThreadA([&]() {
+			for (int i = 0; i < 5000; i++)
+				Signal.Connect(IncrementI);
+		});
+		std::thread ThreadB([&]() {
+			for (int i = 0; i < 5000; i++)
+				Signal.Connect(IncrementI);
+		});
+
+		ThreadA.join();
+		ThreadB.join();
+
+		// Should call the IncrementI lambda 10000 times
+		Signal();
+
+		Assert::AreEqual(i, 10000);
 	}
 };
 
